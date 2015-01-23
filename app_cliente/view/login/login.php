203c2af45c9e5218e39cbb27be641a7df3752f0a
Edgard Marquez <edgardma@gmail.com>
2015-01-23 04:32:32 -0500

    Aplicación cliente PHP

---
+++ app_cliente/view/login/login.php
@@ -0,0 +1,20 @@
+<section id="contact-page" class="container">
+    <div class="row-fluid">
+        <div class="span8">
+            <h4>Autenticaci&oacute;n de usuario</h4>
+            <div class="status alert alert-success" style="display: none"></div>
+
+            <form id="main-contact-form" name="contact-form" method="post" action="">
+              <div class="row-fluid">
+                <div class="span5">
+                    <label>Correo electr&oacute;nico</label>
+                    <input type="text" name="txtCorreo" id="txtCorreo" class="input-block-level" required="required" placeholder="Tu correo electr&oacute;nico">
+                    <label>Contrase&ntilde;a</label>
+                    <input type="password" name="txtClave" id="txtClave" class="input-block-level" required="required" placeholder="Tu clave de acceso">
+                </div>
+            </div>
+            <input type="submit" name="btnIngresar" id="btnIngresar" class="btn btn-primary btn-large pull-left" value="Ingresar">
+            </form>
+        </div>
+    </div>
+</section>
