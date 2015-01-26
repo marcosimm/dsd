d73ffe737cc7c059db4a4004be93bb6c34ae3915
Edgard Marquez <edgardma@gmail.com>
2015-01-25 21:38:14 -0500

    Cambios

--- nodejs_sunat/consultas/consultaruc.js
+++ nodejs_sunat/consultas/consultaruc.js
@@ -1,18 +1,18 @@
 var mongo = require('mongodb');
- 
+
 var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
- 
+
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 db = new Db('sunatdb', server);
- 
+
 console.log("Inicio");
- 
+
 db.open(function(err, db) {
             if(!err) {
                 console.log("Connected to 'sunatdb' database");
-                db.collection('sunat', {strict:true}, 
+                db.collection('sunat', {strict:true},
                     function(err, collection) {
                         if (err) {
                             console.log("The 'sunat' collection doesn't exist. Creating it with sample data...");
@@ -21,22 +21,22 @@ db.open(function(err, db) {
                     });
             }
         });
- 
+
 exports.findById = function(req, res) {
                         var id = req.params.id;
                         console.log('Recuperando datos: ' + id);
                         db.collection('sunat', function(err, collection) {
-                            /*collection.findOne({'_id':new BSON.ObjectID(id)}, 
+                            /*collection.findOne({'_id':new BSON.ObjectID(id)},
                             function(err, item) {
                                 res.send(item);
                             });*/
-                            collection.findOne({'ruc':id}, 
+                            collection.findOne({'ruc':id},
                             function(err, item) {
                                 res.send(item);
                             });
                         });
                     };
- 
+
 exports.findAll =   function(req, res) {
                         console.log('Recuperando todos');
                         db.collection('sunat', function(err, collection) {
@@ -45,11 +45,11 @@ exports.findAll =   function(req, res) {
                             });
                         });
                     };
- 
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 // Populate database with sample data -- Only used once: the first time the application is started.
 // You'd typically not find this code in a real-life app, since the database would already exist.
-var populateDB = function() { 
+var populateDB = function() {
     var sunat = [
             {
                 ruc : "12345678901",
@@ -60,7 +60,11 @@ var populateDB = function() {
                 estado: "ACTIVO",
                 direccion: "Av. Sin nombre No.9999 - Urb. Las Praderas - San Juan de Luirgancho",
                 telefono1: "99999999",
-                anexo1: "1234"
+                anexo1: "1234",
+                represetantes: [
+                {documento: "DNI", numero: "10453112", nombre: "Ana Morales", cargo: "Director Ejecutivo", fechadesde: "10/10/2005"},
+                {documento: "DNI", numero: "10453113", nombre: "Luisa San Martin", cargo: "Gerente General", fechadesde: "11/10/2008"}
+                ]
             },
             {
                 ruc : "12345678902",
@@ -71,7 +75,11 @@ var populateDB = function() {
                 estado: "ACTIVO",
                 direccion: "Av. Con Nombre No.888 - Urb. Los Pino - Comas",
                 telefono1: "8888888",
-                anexo1: "5678"
+                anexo1: "5678",
+                represetantes: [
+                {documento: "DNI", numero: "10453112", nombre: "Ana Morales", cargo: "Director Ejecutivo", fechadesde: "10/10/2005"},
+                {documento: "DNI", numero: "10453113", nombre: "Luisa San Martin", cargo: "Gerente General", fechadesde: "11/10/2008"}
+                ]
             },
             {
                 ruc : "12345678903",
@@ -82,7 +90,11 @@ var populateDB = function() {
                 estado: "ACTIVO",
                 direccion: "Av. Con nombre No.888 - Urb. Dulanto - Callao",
                 telefono1: "8888888",
-                anexo1: "5678"
+                anexo1: "5678",
+                represetantes: [
+                {documento: "DNI", numero: "10453114", nombre: "Miguel Cordova", cargo: "Gerente General", fechadesde: "10/10/2005"},
+                {documento: "DNI", numero: "10453115", nombre: "Pablo Rodriguez", cargo: "Administrador", fechadesde: "11/10/2008"}
+                ]
             },
             {
                 ruc : "12345678904",
@@ -93,7 +105,11 @@ var populateDB = function() {
                 estado: "ACTIVO",
                 direccion: "Av. Los Constructores No.777 - San Luis",
                 telefono1: "44444444",
-                anexo1: "89777"
+                anexo1: "89777",
+                represetantes: [
+                {documento: "DNI", numero: "10453116", nombre: "Victor Gomez", cargo: "Apoderado", fechadesde: "10/10/2005"},
+                {documento: "DNI", numero: "10453117", nombre: "Julio Enriquez", cargo: "Presidente de Directorio", fechadesde: "11/10/2008"}
+                ]
             },
             {
                 ruc : "12345678905",
@@ -104,10 +120,14 @@ var populateDB = function() {
                 estado: "ACTIVO",
                 direccion: "Av. Las Flores No.1450 - Urb. Los Nogales - Miraflores",
                 telefono1: "879711111",
-                anexo1: "121545"
+                anexo1: "121545",
+                represetantes: [
+                {documento: "DNI", numero: "10453118", nombre: "Manuel Caceres", cargo: "Gerente General", fechadesde: "10/10/2005"},
+                {documento: "DNI", numero: "10453119", nombre: "Oscar Villar", cargo: "Administrador", fechadesde: "11/10/2008"}
+                ]
             }];
- 
+
         db.collection('sunat', function(err, collection) {
             collection.insert(sunat, {safe:true}, function(err, result) {});
         });
-    }; 
+    };
